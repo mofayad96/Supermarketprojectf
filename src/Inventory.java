99ff79ef7f94a1fae8bda92e78f5fb094a9627be
mofayad96 <117763149+mofayad96@users.noreply.github.com>
2023-05-18 00:49:04 +0300

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,1 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+    private Map<Product, Integer> stock;
+
+    public Inventory() {
+        this.stock = new HashMap<>();
+    }
+
+    public void addProduct(Product product, int quantity) {
+        int currentQuantity = stock.getOrDefault(product, 0);
+        stock.put(product, currentQuantity + quantity);
+    }
+
+    public void removeProduct(Product product, int quantity) {
+        int currentQuantity = stock.getOrDefault(product, 0);
+
+        if (currentQuantity - quantity <= 0) {
+            stock.remove(product);
+        } else {
+            stock.put(product, currentQuantity - quantity);
+        }
+    }
+
+    public int getProductQuantity(Product product) {
+        return stock.getOrDefault(product, 0);
+    }
+
+    public void displayInventory() {
+        System.out.println("Inventory:");
+
+        for (Map.Entry<Product, Integer> entry : stock.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+
+            System.out.println(product.getName() + " - Quantity: " + quantity);
+        }
+    }
+}
 
