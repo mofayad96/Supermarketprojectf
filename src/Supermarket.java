38201d8ea898feac634f816ff6e4b0102981e0c0
mofayad96 <117763149+mofayad96@users.noreply.github.com>
2023-05-18 06:13:39 +0300

    Update Supermarket.java

--- src/Supermarket.java
+++ src/Supermarket.java
@@ -1,54 +1,64 @@
- import java.time.LocalDate;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 public class Supermarket {
- 
+
     private List<Product> products;
     private List<Client> Clients;
     private List<Staffmember> StaffMembers;
     private List<Staffcard> Staffcards;
     private List<Invoice> invoices;
-    private HashMap<String, Integer> product = new HashMap<>();
 
-public Supermarket() {
+    public Supermarket() {
         this.products = new ArrayList<>();
         this.Clients = new ArrayList<>();
         this.StaffMembers = new ArrayList<>();
         this.Staffcards = new ArrayList<>();
         this.invoices = new ArrayList<>();
     }
- public void addproduct(Product product) {
+
+    public void addproduct(Product product) {
         this.products.add(product);
     }
 
     public void removeProduct(Product product) {
         this.products.remove(product);
     }
- public void addInvoices(Invoice invoice) {
-        this.invoices.add(invoice);
+
+    public void addClient(Client client) {
+        this.Clients.add(client);
     }
 
-    public void removeInvoices(Invoice invoice) {
-        this.invoices.remove(invoice);
+    public void removeClient(Client client) {
+        this.Clients.remove(client);
+
     }
- public void addStaffmember(Staffmember staffmember) {
+
+    public void addStaffmember(Staffmember staffmember) {
         this.StaffMembers.add(staffmember);
     }
 
     public void removeStaffmembers(Staffmember staffmember) {
         this.StaffMembers.remove(staffmember);
     }
- public void addStaffcard(Staffcard staffcard) {
+
+    public void addStaffcard(Staffcard staffcard) {
         this.Staffcards.add(staffcard);
     }
 
     public void removeStaffcard(Staffcard staffcard) {
         this.Staffcards.remove(staffcard);
     }
- 
-public int sellProduct(Product product, int quantity, Client client,Staffmember staffmember,PointOfSale pointOfSale) {
+
+    public void addInvoices(Invoice invoice) {
+        this.invoices.add(invoice);
+    }
+
+    public void removeInvoices(Invoice invoice) {
+        this.invoices.remove(invoice);
+    }
+
+    public int sellProduct(Product product, int quantity, Client client, Staffmember staffmember, PointOfSale pointOfSale) {
         if (product.getQuantity() < quantity) {
             System.out.println("Sorry, " + product.getName() + " is out of stock.");
             return product.getQuantity();
@@ -59,32 +69,31 @@ public int sellProduct(Product product, int quantity, Client client,Staffmember 
             return 0;
         }
 
-        {
+        int soldQuantity = quantity;
+        product.setQuantity(product.getQuantity() - soldQuantity);
+        if (product.getQuantity() < 10) {
+            System.out.println("Alert: The quantity of " + product.getName() + " is now less than 10.");
+        }
 
         double initialPrice = product.getSellPrice() * quantity;
         double discount = calculateDiscount(initialPrice, client, product);
         double finalPrice = initialPrice - discount;
 
-            Invoice invoice = new Invoice(LocalDate.now().toString(), LocalDate.now().toString(), initialPrice, finalPrice, discount, staffmember.getId(), pointOfSale.getId(), quantity);
-            addInvoices(invoice);
+        Invoice invoice = new Invoice(LocalDate.now().toString(), LocalDate.now().toString(), initialPrice, finalPrice, discount, staffmember.getId(), pointOfSale.getId(), quantity);
+        addInvoices(invoice);
 
         System.out.println("Sold " + quantity + " units of " + product.getName() + " to " + client.getName() + ".");
         System.out.println("Initial Price: " + initialPrice + " L.E.");
         System.out.println("Discount: " + discount + " L.E.");
         System.out.println("Final Price: " + finalPrice + " L.E.");
-        System.out.println("point of sale ID: "+pointOfSale.getId()+" || "+"Location: "+pointOfSale.getLocation());
-        System.out.println("Staffmember ID :"+staffmember.getId());
+        System.out.println("point of sale ID: " + pointOfSale.getId() + " || " + "Location: " + pointOfSale.getLocation());
+        System.out.println("Staffmember ID :" + staffmember.getId());
         System.out.println("Thank you for using our store ☺");
-    }
-
-
-    int soldQuantity = quantity;
-
         product.setQuantity(product.getQuantity() - soldQuantity);
         client.setPurchaseAmountInMonth(client.getPurchaseAmountInMonth() + (int) (soldQuantity * product.getSellPrice()));
         return soldQuantity;
     }
- double calculateDiscount(double initialPrice, Client client, Product product) {
+    double calculateDiscount(double initialPrice, Client client, Product product) {
         double discount = 0;
         if (isGolden(client)) {
             if (LocalDate.now().equals(client.getBirthday())) {
@@ -96,10 +105,14 @@ public int sellProduct(Product product, int quantity, Client client,Staffmember 
             }
 
         }
+        if (LocalDate.now().equals(LocalDate.of(2023, 5, 18))) {
+            discount += initialPrice * 0.87;
+        }
+
         return discount;
     }
-  private boolean isGolden(Client client) {
+
+    private boolean isGolden(Client client) {
         return LocalDate.now().minusYears(5).isAfter(client.getStartDate());
     }
-
 }
